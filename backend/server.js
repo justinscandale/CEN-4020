138c96b7ae7db676c24faef6a8958ac1cd4234c5
Huu Dang Pham <danghuupham@usf.edu>
2025-02-15 17:16:49 -0500

    fix conflic

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const receiptRoutes = require("./routes/receiptRoutes");
 const app = express();
 
 // Middleware
-app.use(cors()); // Add this line
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
